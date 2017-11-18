d7347033f028d8d3276d8c2f2064a15e9150a69a
Francesco Gianni <francesco.gianni@live.it>
2017-11-18 11:43:56 +0100

    init

---
+++ src/main/java/com/teamtreehouse/giflib/service/CategoryServiceImpl.java
@@ -0,0 +1,31 @@
+package com.teamtreehouse.giflib.service;
+
+import com.teamtreehouse.giflib.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+
+    @Override
+    public List<Category> findAll() {
+        return null;
+    }
+
+    @Override
+    public Category findById() {
+        return null;
+    }
+
+    @Override
+    public void save(Category category) {
+
+    }
+
+    @Override
+    public void delete(Category category) {
+
+    }
+}
